96f5258a12f41781142190f9d4b764eca3026132
pohjala1nen <58693495+pohjala1nen@users.noreply.github.com>
2019-12-18 13:51:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
 $(document).ready(function(){
   
-  function passWord() {
-var testV = 1;
-var pass1 = prompt('Please Enter Your Password',' ');
-while (testV < 3) {
-if (!pass1) 
-history.go(-1);
-if (pass1.toLowerCase() == "tsirptsirp") {
-alert('You Got it Right!');
-window.open('https://competent-gates-7f6f9f.netlify.com/');
-break;
-} 
-testV+=1;
-var pass1 = 
-prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
-}
-if (pass1.toLowerCase()!="password" & testV ==3) 
-history.go(-1);
-return " ";
-} 
-  
     $("#list-items").html(localStorage.getItem("listItems"));
     
     $(".add-items").submit(function(event){
