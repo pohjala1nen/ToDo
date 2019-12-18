1c06fafecf7e19e85beddee8e5138c86ce67ab37
pohjala1nen <58693495+pohjala1nen@users.noreply.github.com>
2019-12-18 12:46:43 +0200

    Add files via upload
    Initial upload

---
+++ dist/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  
+  
+    $("#list-items").html(localStorage.getItem("listItems"));
+    
+    $(".add-items").submit(function(event){
+      
+      event.preventDefault();
+      
+      var item = $("#todo-list-item").val();
+      
+      if (item){
+        $("#list-items").append(
+        "<li><input class='checkbox' type='checkbox' />" +
+          item + " <a class='remove'>Remove</a><hr></li>"
+        );
+        
+      localStorage.setItem("listItems", $("#list-items").html());
+        $("#todo-list-item").val("");
+        
+        
+      }
+      else
+        alert("Input box can't be empty");
+      
+    });
+    
+    
+    $(document).on("change", ".checkbox", function(){
+      if($(this).attr("checked")){
+        $(this).removeAttr("checked");
+      }
+      else{
+          $(this).attr("checked", "checked");
+      }
+      
+      $(this).parent().toggleClass("completed");
+      
+      localStorage.setItem("listItem", $("#list-items").html());
+      
+    })
+    
+    $(document).on("click", ".remove", function (){
+      $(this).parent().remove();
+      localStorage.setItem("listItem", $("#list-items").html());
+    });
+  
+  
+  });
