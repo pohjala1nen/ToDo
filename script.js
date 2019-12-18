2457c1f6499cfd1cae867dec8604f99f709adb19
pohjala1nen <58693495+pohjala1nen@users.noreply.github.com>
2019-12-18 13:57:25 +0200

    Update script.js
    26 riville uutta

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 $(document).ready(function(){
+  function passWord() 
+    {
+             var testV = 1;
+             var pass1 = prompt('Please Enter Your Password',' ');
+             while (testV < 3) 
+             {
+                    if (!pass1) 
+                    {
+                           history.go(-1);
+                           var pass1 = prompt('Access Denied - Password Incorrect, Please Try     
+                           Again.','Password');
+                    }
+                    else if (pass1.toLowerCase() == "letmein") 
+                    {
+                         alert('You Got it Right!');
+                         window.open('protectpage.html');
+                         break;
+                    } 
+                    testV+=1;
+
+              }
+              if (pass1.toLowerCase()!="password" & testV ==3) 
+              history.go(-1);
+              return " ";
+    } 
   
     $("#list-items").html(localStorage.getItem("listItems"));
     
